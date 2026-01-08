a6e614278f25ffb3f26a308c04832a3091c7fa39
Nahuel Coronel <nahucarc2@gmail.com>
2026-01-08 20:05:53 -0300

    Initial commit

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,14 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TechnicianController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']) ;
 Route::post('/create-admin', [AuthController::class, 'createAdmin']);
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::post('/logout', [AuthController::class, 'logout'])->middleware(middleware: 'auth:sanctum');
 Route::get('/user', [AuthController::class, 'user'])->middleware('auth:sanctum');
+
+
+Route::resource('technician', TechnicianController::class);
